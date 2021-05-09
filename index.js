7754750a751e4b2f826666e36b7ed95248632536
user.name "barkhoum GUESMIA <smguesmia@gmail.com>
2021-05-09 02:55:47 +0200

    ajout code Html, Css & Js

--- index.js
+++ index.js
@@ -23,8 +23,8 @@
 // FaireUneChose('des pates');
 
 /////)/bouton
-const btn = document.querySelector("btn");
-const img = document.querySelector("img");
+const btn = document.querySelector("#btn");
+const img = document.getElementById("img");
 
 btn.addEventListener("click", () => {
     img.classList.toggle("show");
@@ -38,16 +38,49 @@ mouseEvent.addEventListener("mousemove", (e) => {
     // console.log(e);
     horizontal.innerHTML = e.x;
     vertical.innerHTML = e.y;
-});
-let age = 40;
-console.log(age + " ans");
-const prenom = " Barkhoum";
-console.log(prenom);
-const nom = " Guesmia";
-console.log(nom);
-console.log("je m appelle" + prenom + nom + " et j ai " + age + " ans.");
+    mouseEvent.style.left = (e.x / window.innerWidth) * 100 + "%";
 
+    if (e.x > 500) {
+        document.body.style.filter = "blur(3px)";
+    } else {
+        document.body.style.filter = "none ";
+    }
+});
+
+// // ***************
 // const dateNaissance = (date) => {
 //     console.log('date de naissance' + date);
 // };
-// dateNaissance(`23 /02/ 1981`);
+// dateNaissance(" 23 / 02 / 1981");
+// const nom = ' GUESMIA';
+// const prenom = ' Barkhoum'
+// const age = '40 ans'
+
+// console.log("je m appelle" + prenom + nom + " et j ai " + age + " ans.");
+// **** ** ** ** ** *
+document.getElementById("input").addEventListener("input", (e) => {
+    vertical.innerHTML = e.target.value;
+});
+
+//********************* */
+const theme = document.querySelectorAll(".theme");
+
+theme.forEach((item) => {
+    item.addEventListener("click", (e) => {
+        console.log(e.target.id);
+        document.body.classList.remove("darkTheme", "salmonTheme", "purpleTheme");
+        switch (e.target.id) {
+            case "dark":
+                document.body.classList.add("darkTheme");
+                break;
+            case "salmon":
+                document.body.classList.add("salmonTheme");
+                break;
+            case "purple":
+                document.body.classList.add("purpleTheme");
+                break;
+            default:
+                null;
+        }
+    });
+});
